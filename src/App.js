17b59794de49b644efeb12a2c69b8e00a49f7387
Chute <chuteec@8c8590b93e73.ant.amazon.com>
2019-07-15 13:22:41 -0400

    started customer components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-import React from 'react';
-import logo from './assets/img/logo/AWS_logo_RGB_REV.svg';
+import React from "react";
+import logo from "./assets/img/logo/AWS_logo_RGB_REV.svg";
 
 // import { Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="react-logo" />
-        <p style={{marginTop: "100px"}}>
-          Edit <code>/src/App.js</code> and save to reload.
-        </p>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="react-logo" />
+				<p style={{ marginTop: "100px" }}>
+					Edit <code>/src/App.js</code> and save to reload.
+				</p>
 
-        <div className="App-nav">
-          <a className="App-link" href="/login" style={{marginRight: "20px"}}>Login</a>
-          <a className="App-link" href="/offerings" style={{marginRight: "20px"}}>Offerings</a>
-          <a className="App-link" href="/testing" style={{marginRight: "20px"}}>Testing</a>
-          <a className="App-link" href="/404">(404)</a>
-        </div>
+				<div className="App-nav">
+					<a
+						className="App-link"
+						href="/login"
+						style={{ marginRight: "20px" }}
+					>
+						Login
+					</a>
+					<a
+						className="App-link"
+						href="/offerings"
+						style={{ marginRight: "20px" }}
+					>
+						Offerings
+					</a>
+					<a
+						className="App-link"
+						href="/testing"
+						style={{ marginRight: "20px" }}
+					>
+						Testing
+					</a>
+					<a
+						className="App-link"
+						href="/customer"
+						style={{ marginRight: 20 }}
+					>
+						Customer
+					</a>
+					<a className="App-link" href="/404">
+						(404)
+					</a>
+				</div>
 
-        {/* <Route render={({ history }) => (
+				{/* <Route render={({ history }) => (
           <button type='button' onClick={() => { history.push('/testing') }}>Test Page</button>
         )} />
 
         <Route render={({ history }) => (
           <button type='button' onClick={() => { history.push('/login') }}>Login Page</button>
         )} /> */}
-      </header>
-    </div>
-  );
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import './scss/style.scss';
+import "./scss/style.scss";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import Login from "./js/components/Login";
 import Testing from "./js/components/Testing";
 import Notfound from "./js/components/Notfound";
+import Customer from "./js/components/Customer";
 // import Offerings from "./js/components/offerings/Offerings";
 
 const routing = (
-  <Router>
-    {/* <Link to="/"> Home </Link>|
+	<Router>
+		{/* <Link to="/"> Home </Link>|
     <Link to="/login"> Login </Link>|
     <Link to="/testing"> Testing </Link>| */}
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/testing" component={Testing} />
-      {/* <Route path="/offerings" component={Offerings} /> */}
-      <Route component={Notfound} />
-    </Switch>
-  </Router>
+		<Switch>
+			<Route exact path="/" component={App} />
+			<Route path="/login" component={Login} />
+			<Route path="/testing" component={Testing} />
+			<Route path="/customer" component={Customer} />
+			{/* <Route path="/offerings" component={Offerings} /> */}
+			<Route component={Notfound} />
+		</Switch>
+	</Router>
 );
 
 // entry point for react rendering
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // default
 // ReactDOM.render(<App />, document.getElementById('root'));
