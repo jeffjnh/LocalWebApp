4e8105df851ac27323c0af89eb77e728e4b72272
Miffy Chen <mchen15@bu.edu>
2019-07-15 13:12:39 -0400

    Small patch #2 (cont)

---
+++ build/precache-manifest.dc8fc0ce5153eb14877c4f47dfe2f3ea.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311c96d66495d7bb44e07c5b8ec42ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c614d1e46f5c0a45e48",
+    "url": "/static/css/main.94ae922b.chunk.css"
+  },
+  {
+    "revision": "f0fd6111e2d0c2e8abe2",
+    "url": "/static/js/2.48c6ced3.chunk.js"
+  },
+  {
+    "revision": "9c614d1e46f5c0a45e48",
+    "url": "/static/js/main.3cc66b06.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4e046a5c3302efc453fa8b7bd602e25",
+    "url": "/static/media/1_align_gray.d4e046a5.png"
+  },
+  {
+    "revision": "8c5568a905ca6decf1db876b53d2a211",
+    "url": "/static/media/2_launch_gray.8c5568a9.png"
+  },
+  {
+    "revision": "4c205091af6b4577c4652f8af5017899",
+    "url": "/static/media/3_scale_gray.4c205091.png"
+  },
+  {
+    "revision": "8b15caaaa20f5b1c8be4c963532f2daf",
+    "url": "/static/media/4_optimize_gray.8b15caaa.png"
+  },
+  {
+    "revision": "0ef4572ed94a0db96ad1d569fb087158",
+    "url": "/static/media/AWS_logo_RGB_REV.0ef4572e.svg"
+  },
+  {
+    "revision": "1ecf1ae090c4add6e06334ebe3fde824",
+    "url": "/static/media/AmazonEmberMono_Rg.1ecf1ae0.ttf"
+  },
+  {
+    "revision": "76666a38ff1cda13e51186b795623090",
+    "url": "/static/media/AmazonEmber_Rg.76666a38.ttf"
+  },
+  {
+    "revision": "7b91480c89665b6377acacdec0252203",
+    "url": "/static/media/AmazonEmber_Th.7b91480c.ttf"
+  },
+  {
+    "revision": "9c07e3e835ee563aaa1b33e92cb9c770",
+    "url": "/static/media/BG-Pattern_Levels_Squid-Ink.9c07e3e8.png"
+  },
+  {
+    "revision": "10dee1d125cfef82ee48b8197c8e705f",
+    "url": "/static/media/delivery_kit.10dee1d1.svg"
+  },
+  {
+    "revision": "07a98bd1c4cef274d55757970de34896",
+    "url": "/static/media/peciSage.sad.07a98bd1.png"
+  },
+  {
+    "revision": "0b6d6d19e887fe14289323a11674c59b",
+    "url": "/static/media/sales_kit.0b6d6d19.svg"
+  }
+]);
