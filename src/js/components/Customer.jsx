ab29622542d8dff5d5bb740dfc08792141b477cd
Jeff Johansen <johajeff@amazon.com>
2019-07-29 13:40:09 -0400

    Removing excess logging...whoops

--- src/js/components/Customer.jsx
+++ src/js/components/Customer.jsx
@@ -340,11 +340,8 @@ class Customer extends Component {
 										{
 											id: "total_opportunity",
 											Header: "Total Opportunity",
-											accessor: value => {
-												console.log(parseInt(value.total_opportunity).toLocaleString(value.list_price_currency));
-												return parseInt(value.total_opportunity).toLocaleString(undefined, {style:"currency", currency:value.list_price_currency});
-											}
-												// "$" + value.total_opportunity
+											accessor: value =>
+												parseInt(value.total_opportunity).toLocaleString(undefined, {style:"currency", currency:value.list_price_currency})
 										}
 									]
 								}
